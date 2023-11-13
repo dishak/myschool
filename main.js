9f4d1027f46d7e96d6885043c566089df89289f5
unknown <disha.karnataki@gmail.com>
2023-11-13 16:31:27 +0100

    third

--- main.js
+++ main.js
@@ -2,3 +2,21 @@ window.addEventListener('scroll',()=>{
     document.querySelector('nav').classList.toggle("nav_color_change_onscroll",window.scrollY>100)
     
 })
+
+// show and hide faq section
+let faqs = document.querySelectorAll(".faq");
+
+faqs.forEach(faq=>{
+    faq.addEventListener('click',()=>{
+        faq.classList.toggle('open')
+        const icon=faq.querySelector('.faq__icon i')
+       if(icon.className ==='fa-solid fa-plus')
+       {
+        icon.className="fa-solid fa-minus"
+       }
+       else
+       {
+        icon.className="fa-solid fa-plus"
+       }
+    })
+})
